1a64965785464ffe63f40f4020c19adc6005a5ea
Nelson C <ncdev2015@gmail.com>
2024-03-27 21:57:10 -0300

    initial commit

---
+++ public/main.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const grid  = new THREE.GridHelper(1000, 1000);
+scene.add(grid);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+
+cube.position.set(1,1,1);
+
+scene.add( cube );
+
+camera.position.set(1, 1, 5);
+
+renderer.render( scene, camera );
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	stats.update();
+
+	renderer.render( scene, camera );
+}
+
+animate();
